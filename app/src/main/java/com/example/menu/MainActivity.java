b532b6dd1cfa7dd7d0820c02fd869b31d0236a04
Đạt Bùi Hoàng <99buidat@gmail.com>
2021-08-12 11:35:17 +0700

    cm1

--- app/src/main/java/com/example/menu/MainActivity.java
+++ app/src/main/java/com/example/menu/MainActivity.java
@@ -14,6 +14,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private TextView tv;
+    private String c = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
